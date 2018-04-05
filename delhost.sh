27495eabf08d087de88296569da414b7996e62dc
phanuwat3955 <38115024+phanuwat3955@users.noreply.github.com>
2018-04-05 21:10:05 +0000

    Add files via upload

--- delhost.sh
+++ delhost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-payload="/etc/squid/payload.txt"
+payload="/etc/squid3/payload.txt"
 tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-10s\n' "Remover Host do Squid3" ; tput sgr0
 if [ ! -f "$payload" ]
 then
@@ -22,11 +22,11 @@ else
 			grep -v "^$host" $payload > /tmp/a && mv /tmp/a $payload
 			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "Arquivo $payload atualizado, o domínio foi removido com sucesso:" ; tput sgr0
 			tput setaf 3 ; tput bold ; echo "" ; cat $payload ; echo "" ; tput sgr0
-			if [ ! -f "/etc/init.d/squid" ]
+			if [ ! -f "/etc/init.d/squid3" ]
 			then
-				service squid reload
+				service squid3 reload
 			else
-				/etc/init.d/squid reload
+				/etc/init.d/squid3 reload
 			fi	
 			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "O Proxy Squid3 foi recarregado com sucesso!" ; echo "" ; tput sgr0
 			exit 1
