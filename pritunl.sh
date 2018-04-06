fb279af0520f65bc30d5a3d670400b8390cd4147
Panuwatbank <35716517+Panuwatbank@users.noreply.github.com>
2018-04-06 18:09:12 +0700

    Update pritunl.sh

--- pritunl.sh
+++ pritunl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd
+sudo apt-get update && sudo apt-get upgrade
+cd
 # install "UBUNTU"
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.6.list
 echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
@@ -12,11 +14,16 @@ systemctl start pritunl mongod
 systemctl enable pritunl mongod
 cd
 # install "Ubuntu Trusty"
-sudo tee -a /etc/apt/sources.list.d/mongodb-org-3.6.list << EOF deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse EOF 
-sudo tee -a /etc/apt/sources.list.d/pritunl.list << EOF deb http://repo.pritunl.com/stable/apt trusty main EOF 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo apt-get update 
-sudo apt-get --assume-yes install pritunl mongodb-org 
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
+sudo apt-get update
+
+sudo apt-get install python-software-properties pritunl mongodb-org
+
+
+
+
+
 sudo service pritunl start
-sudo pritunl setup-key
